d92c12c2cba4ede83cc8d043f79e9dafccec97ce
arora-jasjot <jasjotsinghchugh@gmail.com>
2022-07-06 00:22:18 +0530

    Product card

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -3,6 +3,10 @@ import styles from './Shop.module.css';
 import Navbar from '../../Components/Navbar/Navbar';
 import Footer from '../../Components/Footer/Footer';
 import bgImg from '../../Media/shop-bg.png'
+import { FaStar, FaStarHalf } from 'react-icons/fa';
+import image1 from '../../Media/Shop/01.png'
+import image2 from '../../Media/Shop/02.png'
+import image3 from '../../Media/Shop/03.png'
 
 const Shop = () => {
     return (
@@ -15,12 +19,21 @@ const Shop = () => {
                 </div>
                 <div className='content-section'>
                     <div className={styles.productCard}>
-                        <div className={styles.productImage}></div>
+                        <div className={styles.rating}>
+                            <FaStar />
+                            <FaStar />
+                            <FaStar />
+                            <FaStar />
+                            <FaStarHalf />
+                        </div>
+                        <div className={styles.productImage}>
+                            <img src={image1} alt="image1" />
+                        </div>
                         <div className={styles.productBody}>
                             <div className={styles.productLabel}>Discount : 10%</div>
                             <div className={styles.productName}>XXX Body Whey Protien</div>
                             <div className={styles.productDescription}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </div>
-                            <div className={styles.productPrice}>₹ 3500</div>
+                            <div className={styles.productPrice}>₹ 3600 <span>₹ 4000</span></div>
                         </div>
                         <div className={styles.orderNowBtn}>Order Now</div>
                     </div>
